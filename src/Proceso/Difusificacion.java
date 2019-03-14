99c2678918f61b1e0e04a89a720a12646f785ba5
jess-24 <jess-24@outlook.com>
2019-03-14 16:35:26 -0600

    Iniciando proyecto

---
+++ src/Proceso/Difusificacion.java
@@ -0,0 +1,8 @@
+package Proceso;
+import java.util.*;
+
+public class Difusificacion {
+    public void inicio(){
+        System.out.println("jess");
+    }
+}
---
+++ src/Proceso/Main.java
@@ -0,0 +1,8 @@
+package Proceso;
+
+public class Main {
+    public static void main(String [] args){
+        Difusificacion d = new Difusificacion();
+        d.inicio();
+    }
+}
