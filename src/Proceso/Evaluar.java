cb1aa2724ba8aafe12d10b43c5a68be9e8a4d7dd
Armando Barron Garcia <madaraushiha54@gmail.com>
2019-03-17 11:02:10 -0600

    Ocupo los archivos :)

--- src/Proceso/Evaluar.java
+++ src/Proceso/Evaluar.java
@@ -1,59 +1,51 @@
 package Proceso;
 
-public class Evaluar
-{
+public class Evaluar {
 
-public static void Evaluer(int entrara_real)
-{
-    int PC1=1,PC2=1,PC3=1;
-    int primera_etiqueda=0,ultima_etiqueta=0;
-    int entrada_real=0;
+    public static void Evaluer(int entrara_real) {
+        int PC1 = 1, PC2 = 1, PC3 = 1;
+        int primera_etiqueda = 0, ultima_etiqueta = 0;
+        int entrada_real = 0;
 
-    String[] puntoss = Consultar_archivo_PC();
+        //String[] puntoss = Consultar_archivo_PC();
 
 
-
-    if(entrada_real==PC1)
-    {
-        PC1=1;
-        PC2=0;
-        PC3=0;
-    }
-	else
-    {
-        if (entrada_real==PC2)
-        {
-            PC1=0;
-            PC2=1;
-            PC3=0;
-        }
-        else
-        {
-            if (entrada_real==PC3)
-            {
-                PC1=0;
-                PC2=0;
-                PC3=1;
-            }
-            else
-            {
-                if (entrada_real<= primera_etiqueda)
-                {
-                PC1=1;
-                PC2=0;
-                PC3=3;
-                }
-				else
-                {
-                    if (entrada_real>= ultima_etiqueta)
-                    {
-                    PC1=0;
-                    PC2=0;
-                    PC3=1;
+        if (entrada_real == PC1) {
+            PC1 = 1;
+            PC2 = 0;
+            PC3 = 0;
+        } else {
+            if (entrada_real == PC2) {
+                PC1 = 0;
+                PC2 = 1;
+                PC3 = 0;
+            } else {
+                if (entrada_real == PC3) {
+                    PC1 = 0;
+                    PC2 = 0;
+                    PC3 = 1;
+                } else {
+                    if (entrada_real <= primera_etiqueda) {
+                        PC1 = 1;
+                        PC2 = 0;
+                        PC3 = 3;
+                    } else {
+                        if (entrada_real >= ultima_etiqueta) {
+                            PC1 = 0;
+                            PC2 = 0;
+                            PC3 = 1;
+                        } else {
+                            //aqui viene lo chido
+                            int pt_izquierda=0, pt_derecha=0,st_izquierda=0,st_derecha=0;
+                            ////extraer los traslapes
+                            if (entrada_real< pt_izquierda)
+                            {
+                                //llamar calcular pendiente
+                            }
+                        }
                     }
                 }
             }
         }
-    }
-}//metodo
+    }//metodo
 }//class
