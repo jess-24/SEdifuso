fa00282fe54073bf09c0795d50c4ae9b3679bb35
jess-24 <jess-24@outlook.com>
2019-03-17 01:05:29 -0600

    difusificacion entre traslape, en proceso...

--- src/Proceso/Difusificacion.java
+++ src/Proceso/Difusificacion.java
@@ -2,7 +2,26 @@ package Proceso;
 import java.util.*;
 
 public class Difusificacion {
-    public void inicio(){
-        System.out.println("jess");
+    int entrada_real;
+    int num_var;
+    double valor_X1;
+    double valor_Y1;
+    double valor_X2;
+    double valor_Y2;
+    private Archivos archivos=new Archivos();
+    public void setEntradaReal(int entrada_real,num_var){
+        this.entrada_real=entrada_real;
+        this.num_var=num_var;
+
+        if(archivos.buscar_ArchivoTraslapes(entrada_real){
+            valor_X2=archivos.getValor_X2();
+            calcularM(archivos.getValor_X1(),1,archivos.getValor_X2(),0);
+            calcularM(archivos.getValor_XX1(),0,archivos.getValor_XX2(),1);
+        }else{
+
+        }
+    }
+    public void calcularM(double valor_X1, int valor_Y1, double valor_X2, double valor_Y2){
+
     }
 }
--- src/Proceso/Main.java
+++ src/Proceso/Main.java
@@ -1,8 +1,46 @@
 package Proceso;
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
 public class Main {
+    private Difusificacion difusicar;
+    public void menu(){
+        Scanner s = new Scanner(System.in);
+        do{
+            System.out.println("1. Evaluar");
+            System.out.println("2. Agregar nuevas variables");
+            System.out.println("2. Editar variables");
+            System.out.println("3. Salir");
+
+            switch (s.nextInt();){
+                case 1:
+                    evaluar();
+                    break;
+            }
+        }while(s<3);
+
+
+    }
+    public void evaluar(){
+        difusicar=new Difusificacion();
+        //leer catidad de variables existentes
+        //int num_var=
+        //pedir valores de cada variable
+        //evaluar cada valor real
+        int entrada_real=45;
+        //for(int x=1; x<=num_var; x++){
+             difusicar.setEntradaReal(entrada_real,num_var);
+        // }
+
+    }
+    public void Obtener_hechos(){
+
+    }
+    public void Agregar_Hechos(){
+
+    }
     public static void main(String [] args){
         Difusificacion d = new Difusificacion();
-        d.inicio();
     }
 }
