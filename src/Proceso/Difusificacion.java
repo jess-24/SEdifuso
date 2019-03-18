9b157e88598a4be7f3bcda998f597e7ce9a36d51
Armando Barron Garcia <madaraushiha54@gmail.com>
2019-03-17 22:33:23 -0600

    Agregue clases de archivos y les meti metodos que aun no funcionan.

--- src/Proceso/Difusificacion.java
+++ src/Proceso/Difusificacion.java
@@ -24,4 +24,26 @@ public class Difusificacion {
     public void calcularM(double valor_X1, int valor_Y1, double valor_X2, double valor_Y2){
 
     }
+
+    public void calculaT(int puntoC1, int puntoC2_1, int puntoC2_2, int puntoC3)
+    {
+        double traslape1,traslape2,traslape3,traslape4;
+        //primer rango de traslape
+        traslape1 = puntoC2_1-((puntoC2_1-puntoC1)*0.75);
+        traslape2 = puntoC1+((puntoC2_1-puntoC1)*0.75);
+        if (puntoC2_2!=0) {
+            //segundo rango
+            traslape3 = puntoC3-((puntoC3-puntoC2_1)*0.75);
+            traslape4 = puntoC2_1+((puntoC3-puntoC2_1)*0.75);
+        }
+        else
+        {
+            //segundo rango con 2 puntos criticos
+            traslape3 = puntoC3-((puntoC3-puntoC2_2)*0.75);
+            traslape3 = puntoC2_2+((puntoC3-puntoC2_2)*0.75);
+        }
+
+
+
+    }
 }
