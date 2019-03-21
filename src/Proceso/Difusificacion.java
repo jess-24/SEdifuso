d6258e2646cfcd15f39e7566948226175297592f
jess-24 <jess-24@outlook.com>
2019-03-20 21:55:29 -0600

    evaluacion en traslapes en proceso...

--- src/Proceso/Difusificacion.java
+++ src/Proceso/Difusificacion.java
@@ -1,30 +1,56 @@
+
 package Proceso;
 
+import java.io.IOException;
 import java.util.*;
 
 public class Difusificacion {
     double entrada_real;
-    int num_var;
+    int num_variable;
     double valor_X1;
     double valor_Y1;
     double valor_X2;
     double valor_Y2;
-    private Archivos archivos = new Archivos();
-
-    public void setEntradaReal(double entrada_real, int num_var) {
+    Maestro maestro;
+    ArrayList<Competencia> variable;
+    /* se crean los rangos de traslapes
+     * Matriz de traslapes
+     * ___________
+     * | 23 | 34 |
+     * | 38 | 45 |
+     * | 56 | 67 |
+     * -----------
+     * */
+    public void setEntradaReal(double entrada_real, int num_variable) {
         this.entrada_real = entrada_real;
-        this.num_var = num_var;
+        this.num_variable = num_variable;
+        double valor_XX1,vallor_XX2;
+        variable=new ArrayList<Competencia>();
+        String etiqueta=null,etiquetaAux=null;
+        int num_etiqueta=0;
+        //num_etiqueta es el numero de traslape en el que se encuentre la entrada real
+        //num_etiqueta=;
+        //Rescatar el valor de X2 Y DE XX1;
+        valor_X2=0;//el final del traslape en el que se encuentra la entrada real;
+        valor_XX1=0;//el inicio del traslape en el que se encuentra la entrada real;
+        //leer e ir comparando con el numero de competencia hasta encontrarlo
+        try {
+            variable = maestro.buscarCompetencia(num_variable);
+            double [][] traslapes=calculaT(variable);
 
-        /*if (archivos.buscar_ArchivoTraslapes(entrada_real)) {
-            valor_X2 = archivos.getValor_X2();
-            calcularM(archivos.getValor_X1(), 1, archivos.getValor_X2(), 0);
-            calcularM(archivos.getValor_XX1(), 0, archivos.getValor_XX2(), 1);
-        } else {
+            valor_X1=variable.get(num_etiqueta).getP2();
+            etiqueta=variable.get(num_etiqueta).getEtiqueta();
+            vallor_XX2=variable.get((num_etiqueta+1)).getP1();
+            etiquetaAux=variable.get((num_etiqueta+1)).getEtiqueta();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            calcularM(valor_X1, 1, valor_X2, 0,entrada_real,etiqueta);
+            calcularM(valor_XX1, 0, valor_XX2, 1,entrada_real,etiquetaAux);
 
-        }*/
     }
     double m;
-    public void calcularM(double valor_X1, int valor_Y1, double valor_X2, double valor_Y2,double entrada_real) {
+    public void calcularM(double valor_X1, int valor_Y1, double valor_X2, double valor_Y2,double entrada_real,String etiqueta) {
         m=(valor_Y2-valor_Y1)/(valor_X2-valor_X1);
         y_grado_membresia=m*entrada_real - (m*valor_X1 +(valor_Y1));
     }
@@ -32,6 +58,7 @@ public class Difusificacion {
         return y_grado_membresia;
     }
     double y_grado_membresia;
+
     public double[][] calculaT(ArrayList<Competencia> puntosCC) {
         double[][] puntosC = new double[8][2];
         double[][] puntosCO = new double[8][2];
